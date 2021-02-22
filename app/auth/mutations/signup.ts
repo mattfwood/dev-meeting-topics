ff9c1e02b014f67ce68b6461a1019a28e7de9319
Matt Wood <matt@cratebind.com>
2021-02-22 14:15:13 -0600

    Fix default role

--- app/auth/mutations/signup.ts
+++ app/auth/mutations/signup.ts
@@ -9,7 +9,7 @@ export default async function signup(input: SignupInputType, { session }: Ctx) {
 
   const hashedPassword = await SecurePassword.hash(password);
   const user = await db.user.create({
-    data: { email: email.toLowerCase(), hashedPassword, role: 'user' },
+    data: { email: email.toLowerCase(), hashedPassword, role: 'USER' },
     select: { id: true, name: true, email: true, role: true },
   });
 
